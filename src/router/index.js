b049a7b2bcb4439adcf3fe43cfabbc1077538e0b
呵呵 <1840494062@qq.com>
2020-05-06 21:52:51 +0800

    完成路由懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,28 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+// 路由懒加载
+// webpack 就会把所有通过 () => import('资源路径') 记载成一个一个独立的代码文件块 chunk
+const Login = () => import('@/views/login/')
+const Home = () => import('@/views/home/')
+const Layout = () => import('@/views/layout/')
+const Article = () => import('@/views/article/')
+const Publish = () => import(/* webpackChunkName: "group-foo" */'@/views/publish/')
+const Image = () => import('@/views/image/')
+const Comment = () => import('@/views/comment/')
+const Fans = () => import(/* webpackChunkName: "group-foo" */'@/views/fans/')
+const Settings = () => import('@/views/settings/')
+
 // 导入组件
-import Login from '@/views/login/'
-import Home from '@/views/home/'
-import Layout from '@/views/layout'
-import Article from '@/views/article'
-import Publish from '@/views/publish'
-import Image from '@/views/image/'
-import Comment from '@/views/comment'
-import Fans from '@/views/fans'
-import Settings from '@/views/settings'
+// import Login from '@/views/login/'
+// import Home from '@/views/home/'
+// import Layout from '@/views/layout'
+// import Article from '@/views/article'
+// import Publish from '@/views/publish'
+// import Image from '@/views/image/'
+// import Comment from '@/views/comment'
+// import Fans from '@/views/fans'
+// import Settings from '@/views/settings'
 
 // 注册路由
 Vue.use(VueRouter)
